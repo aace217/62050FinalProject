8f53a4184e9c995c0f1d2dc9b247ab3d6fd85935
AL1141412 <annliu2022@gmail.com>
2024-11-08 00:53:25 -0500

    finished planned out version of baton trackers

--- FPGA_code/hdl/baton_tracker.sv
+++ FPGA_code/hdl/baton_tracker.sv
@@ -2,22 +2,25 @@
 `default_nettype none
 
 module baton_tracker(
-    input wire [9:0] y_com_in,
+    input wire [10:0] y_com_in,
     input wire measure_in,
     input wire rst_in,
     input wire clk_camera_in,
     output logic change_out
 );
-logic signed [9:0] current_derivative;
-logic signed [9:0] old_derivative;
+logic signed [10:0] current_derivative;
 logic [18:0] delta_t; // counter for making sure that data is valid
 logic signed [10:0] y_diff;
-logic signed [9:0] old_y;
+logic signed [10:0] old_y;
 
-assign y_diff = y_com_in - $signed(old_y); 
+logic [18:0] threshold; // threshold for making sure that data is valid
+// assign threshold = ;
+
+
+assign y_diff = $signed(y_com_in) - $signed(old_y); 
 
 always_ff @(posedge clk_camera_in) begin
-    if(rst_in)begin
+    if (rst_in) begin
         // reset all the variables
         change_out <= 0;
         current_derivative <= 0;
@@ -26,11 +29,17 @@ always_ff @(posedge clk_camera_in) begin
     end else begin
         // implementation of the derivative algorithm
         old_y <= y_com_in;
-        old_derivative <= current_derivative;
+        old_derivative <= y_diff;
         // if the old derivative 
         // wait something like 10k clock cycles to allow the noise to average itself out
-        if()begin
-
+        if (delta_t > threshold) begin
+            if (old_derivative[10] ^ y_diff[10]) begin // if signs are different, change_out is high
+                change_out <= 1; 
+                delta_t <= 0;
+            end else begin
+                change_out <= 0;
+                delta_t <= delta_t + 1;
+            end
         end
 
     end
