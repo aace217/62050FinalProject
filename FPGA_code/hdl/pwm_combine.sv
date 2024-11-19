8b3788d58f19ae1e2f050b243df215bba63bd14f
aace217 <aace217@mit.edu>
2024-11-18 23:13:19 -0500

    Created and verified all fundamental notes. Also, checked that they function with the send_wav.py script

--- FPGA_code/hdl/pwm_combine.sv
+++ FPGA_code/hdl/pwm_combine.sv
@@ -1,4 +1,17 @@
 `timescale 1ns / 1ps
 `default_nettype none
 
+module pwm_combine(   input wire clk_in,
+              input wire rst_in,
+              input wire [7:0] dc_in,
+              output logic sig_out);
+ 
+    logic [31:0] count;
+    counter mc (.clk_in(clk_in),
+                .rst_in(rst_in),
+                .period_in(100000/255), // number of cycles, not the value of the period in ns
+                .count_out(count));
+    assign sig_out = count<dc_in; //very simple threshold check
+endmodule
+
 `default_nettype wire
