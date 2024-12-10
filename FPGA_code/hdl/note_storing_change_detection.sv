90433f838c92182bf04efc447eef682a670d9338
AL1141412 <annliu2022@gmail.com>
2024-12-10 15:52:19 -0500

    no more big chungus module... splitting up note_storing_run_it_back for efficiency and pipelining

---
+++ FPGA_code/hdl/note_storing_change_detection.sv
@@ -0,0 +1,12 @@
+`timescale 1ns / 1ps
+`default_nettype none
+
+module note_storing_change_detection(
+    input wire rst_in,
+    input wire clk_in,
+);
+
+
+
+endmodule
+`default_nettype wire
---
+++ FPGA_code/hdl/note_storing_pixel_addressing.sv
@@ -0,0 +1,12 @@
+`timescale 1ns / 1ps
+`default_nettype none
+
+module note_storing_pixel_addressing(
+    input wire rst_in,
+    input wire clk_in,
+);
+
+
+
+endmodule
+`default_nettype wire
